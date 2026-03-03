236b16cbbe9e7527d72fca614aaab534e9474fc3
hthanhson <leloilonglon999@gmail.com>
2026-03-03 23:45:58 +0700

    fix

--- a.py
+++ a.py
@@ -7,8 +7,8 @@ from streamlit_folium import st_folium
 st.set_page_config(layout="wide")
 st.title("📍 Map trạm")
 
-FILE_PATH = r"e:\du_lieu_tram.xlsx"
-
+# FILE_PATH = r"e:\Test\du_lieu_tram.xlsx"
+FILE_PATH = "du_lieu_tram.xlsx"
 # =========================
 # 1. LOAD
 # =========================
